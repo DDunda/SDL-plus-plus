701323f90767be47e08d239e506f996e85ce0b84
Dunda <50138952+DDunda@users.noreply.github.com>
2021-04-01 18:50:21 +1100

    Added comments for SDL_Error

--- SDL++/SDL++/error.hpp
+++ SDL++/SDL++/error.hpp
@@ -4,10 +4,27 @@
 
 namespace SDL {
 	template<typename... Args>
-	// SetError() unconditionally returns -1.
+
+	/**
+	 *  \brief Set the error message for the current thread
+	 *
+	 *  \return -1, there is no error handling for this function
+	 */
 	static int SetError(SDL_PRINTF_FORMAT_STRING const char* fmt, Args... args) SDL_PRINTF_VARARG_FUNC(1) { return SDL_SetError(fmt, args...); }
 
+	/**
+	 *  \brief Get the last error message that was set
+	 *
+	 * SDL API functions may set error messages and then succeed, so you should
+	 * only use the error value if a function fails.
+	 *
+	 * This returns a pointer to a static buffer for convenience and should not
+	 * be called by multiple threads simultaneously.
+	 *
+	 *  \return a pointer to the last error message that was set
+	 */
 	static const char* GetError() { return SDL_GetError(); }
 
+	// \brief Clear the error message for the current thread
 	static void ClearError() { SDL_ClearError(); }
 }
